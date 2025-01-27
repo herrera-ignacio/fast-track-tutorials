27ad390546e8bcf1196c8e4ea6ca1c66f1a6c4ea
Ignacio Herrera <ignacioromanherrera@gmail.com>
2025-01-27 20:38:08 -0300

    (stripe-one-time-payments)feat: redirectToCheckoutAction

--- stripe-one-time-payments/README.md
+++ stripe-one-time-payments/README.md
@@ -10,4 +10,5 @@
 
 1. `npx create-next-app@latest` to create a new Next app.
 2. (Optional) `pnpm i -D prettier eslint-config-prettier && touch .prettierrc` to use Prettier for formatting.
-3. Delete everything from `app/page.tsx` and add a Buy now button.
+3. Delete everything from `app/page.tsx` and add a Buy now button (#1b4c214).
+4. Create `app/actions.tsx` and add a new `redirectToCheckoutSessionAction` function that handles redirecting to the checkout based on product/price (#f57e85).
---
+++ stripe-one-time-payments/app/actions.tsx
@@ -0,0 +1,37 @@
+"use server";
+
+const CHECKOUT_URL = process.env.NEXT_PUBLIC_CHECKOUT_URL;
+
+export const redirectToCheckoutSessionAction = async (
+  // TIP: Use priceId as you can have support
+  // many prices for the same product
+  priceId: string,
+) => {
+  let checkoutUrl = "/";
+
+  try {
+    // checkoutUrl points to stripe
+    // TIP: You can move this to a separate backend
+    checkoutUrl = await createStripeCheckoutSessionUrl({
+      priceId,
+    });
+  } catch (err) {
+    // checkoutUrl points to our app for an error message
+    checkoutUrl = `${CHECKOUT_URL}?error=true&message=${(err as Error).message}`;
+  }
+
+  return checkoutUrl;
+};
+
+// TIP: Use object instead of array of parameters
+// as it's more easy to maintain as they grow.
+// This function will likely accept more parameters and
+// I don't want to care about their order
+// or updating existing function calls
+const createStripeCheckoutSessionUrl = async ({
+  priceId,
+}: {
+  priceId: string;
+}) => {
+  return "/";
+};
